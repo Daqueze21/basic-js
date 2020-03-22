bfbefbb939cff8f2133bc1db18d0182ba79dd1a7
Dzmitry Bryt <bdv212@yandex.ru>
2020-03-22 18:06:48 +0300

    feat: add solution for transform array and what season tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,27 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+      throw Error();
+    }
+   let arrayToReturn = [];
+   for (let i = 0; i < arr.length; i++) {
+     if (arr[i] == '--double-next') {
+       i == arr.length - 1 ? i++ : arrayToReturn.push(arr[i + 1]);
+       continue;
+     } else if (arr[i] == '--double-prev') {
+       if (i == 0) {
+       } else {
+         arrayToReturn.push(arr[i - 1]);
+       }
+       continue;
+     } else if (arr[i] == '--discard-next') {
+       i == arr.length - 1 ? i++ : i++;
+       continue;
+     } else if (arr[i] == '--discard-prev') {
+       arrayToReturn.pop();
+       continue;
+     }
+     arrayToReturn.push(arr[i]);
+   }
+    return arrayToReturn
     // remove line with error and write your code here
 };
