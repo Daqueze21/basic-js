f7bc81145555be00215337ca056f3b4efe881fc3
Dzmitry Bryt <bdv212@yandex.ru>
2020-03-22 23:18:49 +0300

    feat:add solution to simple chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,35 @@
 const chainMaker = {
+  chain:[],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (!Number.isInteger(position) ||
+      position < 1 ||
+      position > this.chain.length
+    ) {
+      this.chain = [];
+      throw new Error();
+    } else {
+      this.chain.splice(position - 1, 1);
+      return this;
+    }
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let chainToReturn= this.chain.slice(0, this.chain.length);
+    this.chain = [];
+
+    return chainToReturn.join('~~');
   }
+  
 };
 
 module.exports = chainMaker;
