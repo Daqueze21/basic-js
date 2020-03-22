465e9e9965a00a10b7a3ab9a44e00cbed9e62693
Dzmitry Bryt <bdv212@yandex.ru>
2020-03-22 14:26:59 +0300

    feat: add solution for hanoi tower and recursive depth tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,13 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr, depth = 1) {
+        let depthCount = depth;
+
+        for (let i = 0; i < arr.length; i++) {
+           if (Array.isArray(arr[i])) {
+            let numb = this.calculateDepth(arr[i], depth + 1);
+            if(numb>depthCount){ depthCount = numb};
+           }
+        }
+        return depthCount;
     }
 };
